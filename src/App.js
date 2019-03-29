fcf7bb763be072d981bf1c4bba388c48157c96fd
Alen <alenuglik@gmail.com>
2019-03-29 11:14:28 +0100

    4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+              super(props);
+
+              this.state = {
+                input: ''
+              }
+            }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Color />
+          <p>{this.props.color1}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -31,7 +39,8 @@ class Color extends Component {
               this.state = {
                 color1: [],
                 color2: [],
-                colorBlack: "#000000"
+                colorBlack: "#000000",
+                input: ''
               }
             }
 
@@ -39,16 +48,25 @@ class Color extends Component {
               fetch('http://www.colr.org/json/color/random')
               .then(response => response.json())
               .then(data => this.setState({ color1: data.colors[0].hex }));
+
               setTimeout( () => {
                 fetch('http://www.colr.org/json/color/random')
                 .then(response => response.json())
                 .then(data => this.setState({ color2: data.colors[0].hex }));
                       }, 2500);
-          }
+                }
+
+            handleChange(event) {
+              this.setState({
+              input: event.target.value
+                });
+            }
 
             render() {
               return(
                 <div>
+                  <button>Click me</button><br/>
+                  <input value = {this.state.input} onChange = {this.handleChange.bind(this)}/>
                   <p>{this.state.color1}</p>
                   <p>{this.state.color2}</p>
                 </div>
